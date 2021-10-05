04c0229d1366c2601b5d2045a786fb47646bb37b
Joel John <jjohn3593@stu.d214.org>
2021-10-05 12:58:12 -0500

    New Updated Finished Commisson App

--- Commission/ViewController.swift
+++ Commission/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var BasePayLabel: UILabel!
+    @IBOutlet weak var TotalPayLabel: UILabel!
+    @IBOutlet weak var CommisionPayTextField: UITextField!
+   
+    let basePay:Double = 500
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        BasePayLabel.text = "$\(basePay)"
+        TotalPayLabel.text = ""
     }
-
+    @IBAction func whenButtonPrtessed(_ sender: Any)
+    {
+        let dataString = CommisionPayTextField.text!
+        let commisonPay = Double(dataString) ?? 5.0
+        let totalPay = basePay + commisonPay
+        TotalPayLabel.text = "$\(totalPay)"
+        
+        
+    }
+    
 
 }
 
